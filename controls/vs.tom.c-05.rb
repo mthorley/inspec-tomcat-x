41d9502f94b331a329c6a051d5cb76d48d3e6d9f
Matt <matt.thorley@gmail.com>
2021-01-08 22:00:04 +1100

    Tabs

--- controls/vs.tom.c-05.rb
+++ controls/vs.tom.c-05.rb
@@ -3,8 +3,8 @@ control "VS.TOM.C-05" do
   impact 1.0
   title "Ensure default servlet security"
   desc "Default servlet must be: 
-  	- 'Readonly' is set to true to prevent modification or deletion of static resources
-  	- Must not enable directory 'listings' to prevent DOS attacks consuming significant CPU/memory where large number of files exist
+    - 'Readonly' is set to true to prevent modification or deletion of static resources
+    - Must not enable directory 'listings' to prevent DOS attacks consuming significant CPU/memory where large number of files exist
     - Ensure debug is set 0
     - Ensure 'showServerInfo' is set to false to prevent information technology leakage
     - First to load on startup
